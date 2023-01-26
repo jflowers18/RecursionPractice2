8f60a22f998dada37344adcab22a6c8735761f17
jackflowers <jack_flowers22@milton.edu>
2023-01-26 09:32:23 -0500

    Updated recursion with fib

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("In Recursion Main");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter F for Fibonnacci or L for Factorial");
+        String s = input.nextLine();
+
+        //if the input is the letter F then get a number from the user
+        //and fun the fib function
+        //if the input is the letter L then get a number from the user
+        //and fun the factorial function
+        if(s.matches("F")) {
+            System.out.println("Fibonacci Number?");
+            long n = input.nextLong();
+            System.out.println(fib(n));
+        } else if (s.matches("L")) {
+            System.out.println("Factorial Number?");
+            long n = input.nextLong();
+            System.out.println(factorial(n));
+        }
+    }
 
     public static long fib(long n) {
-        if ((n == 0) || (n == 1))
+        if (n == 0 || n == 1)
             return n;
-        else
+        else {
             return fib(n - 1) + fib(n - 2);
+            //System.out.println(l);
+            //return l;
+        }
+    }
+    public static long factorial(long n) {
+        //factorial of 0 and 1 are both 1
+        if (n <= 1)
+            return 1;
+        else {
+            //multiply the current value of n times the factorial of the current number minus one
+            return n * factorial(n -1 );
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Fibonacci Number?");
-        int n = input.nextInt();
-
-
-        System.out.println(fib(n));
-    }
 }
